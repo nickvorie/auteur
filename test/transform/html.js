81547d4c58b7b3e31c0d108bc8310d3b36cdd650
Nick Vorie <nick.vorie@gmail.com>
2019-12-14 20:40:55 -0500

    Rename jest string transformer

--- test/transform/html.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	process(src, filename) {
-		let encoded = Buffer.from(src, "utf-8").toString("hex");
-		return { code: `module.exports = Buffer.from("${encoded}", "hex").toString("utf-8")` };
-	}
-};
--- test/transform/string.transformer.js
+++ test/transform/string.transformer.js
@@ -1,16 +1,5 @@
 /**
  * @author Nicholas Vorie - github.com/nickvorie
- *
- * This is by far the stupiest kludge I've written. I couldn't manage to get Jest to allow to import HTML from a TypeScript file.
- * I tried using jest-transform-stub, which did resolve the errors but would just return an empty string (which cause tests to fail).
- * If there is a more elegant solution to this issue PLEASE let me know and I will be happy to abandon this.
- *
- * To use this, simply ammend your jest.config.json file to include the following:
- * {
- * 	transform: {
- *		"\\.(html|xml|txt)$": "<rootDir>/test/transform/string.transformer.js"
- *	},
- * }
  */
 
 module.exports = {
